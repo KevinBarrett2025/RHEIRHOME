906598c843d5276894c49218f62c663dc427a01e
Kevin Barrett <kevin@rheirhome.com>
2025-05-23 07:12:36 -0400

    Initial Commit

---
+++ RHEIR/RHEIRApp.swift
@@ -0,0 +1,17 @@
+//
+//  RHEIRApp.swift
+//  RHEIR
+//
+//  Created by Kevin Barrett on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct RHEIRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RHEIRTests/RHEIRTests.swift
@@ -0,0 +1,17 @@
+//
+//  RHEIRTests.swift
+//  RHEIRTests
+//
+//  Created by Kevin Barrett on 5/23/25.
+//
+
+import Testing
+@testable import RHEIR
+
+struct RHEIRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
